178eccd19aa044d2e9f1b7932eab3b13aa03232a
Meet <meetshah51245@gmail.com>
2020-09-09 22:42:55 -0400

    Insertion sort code optimization

--- src/classes/Insertion.java
+++ src/classes/Insertion.java
@@ -13,11 +13,15 @@ public class Insertion extends Sort
 		
 		for (int i = 0; i < N; ++i)
 		{
-			for (int j = i - 1; j > 0; --j)
+			for (int j = i; j > 0; --j)
 			{
 				if (less(a[j], a[j - 1]))
 				{
 					exchange(a, j, j - 1);
+				}
+				else
+				{
+					break;
 				}
 			}
 		}
